81c15c4ed50e8b2f09bb42b7feaaa922307d5317
Existing_Human <43016462+Maxwell12345@users.noreply.github.com>
2019-11-12 12:39:23 -0500

    Update Table.cpp

--- Table.cpp
+++ Table.cpp
@@ -12,7 +12,25 @@ typedef struct{
 
 inline void group_prop::setCharge(std::string group)
 {
-	if(group.eq
+	std::string *group_string = (std::string *)malloc(sizeof(std::string) * 18);
+	group_string[0] = "1A";
+	group_string[1] = "2A";
+	group_string[2] = "3B";
+	group_string[3] = "4B";
+	group_string[4] = "5B";
+	group_string[5] = "6B";
+	group_string[6] = "7B";
+	group_string[7] = "8B";
+	group_string[8] = "1B";
+	group_string[9] = "2B";
+	group_string[10] = "3A";
+	group_string[11] = "4A";
+	group_string[12] = "5A";
+	group_string[13] = "6A";
+	group_string[14] = "7A";
+	group_string[15] = "8A";
+	
+	if(group.compare("
 }
 
 template <typename T>
